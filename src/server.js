7d4df1f42375246a1fed4d0b4cdae5669e25d55a
Ardhiyan Abdul Hakim <ardhiyan.hakim.bsn@gmail.com>
2022-04-09 02:31:05 +0700

    Register Albums and Songs plugin onto server

--- src/server.js
+++ src/server.js
@@ -1,10 +1,17 @@
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+const albums = require('./api/albums');
+const songs = require('./api/songs');
+
+const AlbumsService = require('./services/inMemory/AlbumsService');
+const SongsService = require('./services/inMemory/SongsService');
 
 const init = async () => {
+  const albumsService = new AlbumsService();
+  const songsService = new SongsService();
+
   const server = Hapi.server({
     port: 5000,
-    host: 'localhost',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
         origin: ['*'],
@@ -12,7 +19,20 @@ const init = async () => {
     },
   });
 
-  server.route(routes);
+  await server.register([
+    {
+      plugin: albums,
+      options: {
+        service: albumsService,
+      },
+    },
+    {
+      plugin: songs,
+      options: {
+        service: songsService,
+      },
+    },
+  ]);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
