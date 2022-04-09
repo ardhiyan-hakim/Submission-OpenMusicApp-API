3983367c20cfcac739430ec49167719b3358282b
Ardhiyan Abdul Hakim <ardhiyan.hakim.bsn@gmail.com>
2022-04-09 22:56:37 +0700

    Create Table Migration

--- migrations/1649518827575_create-table-albums.js
+++ migrations/1649518827575_create-table-albums.js
@@ -1,7 +1,23 @@
 /* eslint-disable camelcase */
 
-exports.shorthands = undefined;
+exports.up = (pgm) => {
+  pgm.createTable('albums', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    name: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    year: {
+      type: 'INT',
+      notNull: true,
+    },
 
-exports.up = pgm => {};
+  });
+};
 
-exports.down = pgm => {};
+exports.down = (pgm) => {
+  pgm.dropTable('albums');
+};
