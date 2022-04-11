1568fda27916d9644cc03b080b1545d06c5b3053
Ardhiyan Abdul Hakim <ardhiyan.hakim.bsn@gmail.com>
2022-04-11 22:06:03 +0700

    Create Limitation Of Year Input

--- src/validator/albums/schema.js
+++ src/validator/albums/schema.js
@@ -1,8 +1,12 @@
+/* eslint-disable newline-per-chained-call */
+
 const Joi = require('joi');
+
+const currYear = new Date().getFullYear();
 
 const AlbumPayloadSchema = Joi.object({
   name: Joi.string().required(),
-  year: Joi.number().required(),
+  year: Joi.number().integer().min(1800).max(currYear).required(),
 });
 
 module.exports = { AlbumPayloadSchema };
