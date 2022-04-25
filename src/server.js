00c3260c9d5cd3e317749fe38e084a6c9c948b62
Ardhiyan Abdul Hakim <ardhiyan.hakim.bsn@gmail.com>
2022-04-25 07:34:20 +0700

    Implementing Users Plugin on Server

--- src/server.js
+++ src/server.js
@@ -1,18 +1,26 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+
+// Albums
 const albums = require('./api/albums');
-const songs = require('./api/songs');
-
 const AlbumsService = require('./services/postgres/AlbumsService');
-const SongsService = require('./services/postgres/SongsService');
+const AlbumsValidator = require('./validator/albums');
 
-const AlbumsValidator = require('./validator/albums/index');
-const SongsValidator = require('./validator/songs/index');
+// Songs
+const songs = require('./api/songs');
+const SongsService = require('./services/postgres/SongsService');
+const SongsValidator = require('./validator/songs');
+
+// Users
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
 
 const init = async () => {
   const albumsService = new AlbumsService();
   const songsService = new SongsService();
+  const usersService = new UsersService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -37,6 +45,13 @@ const init = async () => {
       options: {
         service: songsService,
         validator: SongsValidator,
+      },
+    },
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
       },
     },
   ]);
